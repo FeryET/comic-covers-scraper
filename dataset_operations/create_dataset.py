7dc29ffaea1ae603d50017a836447e40b2dc22ba
Farhood <farhoodetaati@gmail.com>
2022-03-10 17:42:52 +0330

    adds status code checking for urls

--- dataset_operations/create_dataset.py
+++ dataset_operations/create_dataset.py
@@ -1,5 +1,7 @@
 from http.client import HTTPException
 import logging
+import random
+import time
 import dotenv
 from faker import Faker
 from proxy import ProxyListManager
@@ -19,11 +21,13 @@ def create_image_dataset(metadata_csv, dataset_location, image_size=250):
         try:
             row = df.iloc[row_idx]
             download_image(row, dataset_location, proxy_manager, faker, image_size)
-        except HTTPException as e:
+        except requests.exceptions.RequestException as e:
             logging.info(
                 f"Index {row_idx}: {row['img_url']} has error of type"
                 f" {e.__class__.__qualname__}."
             )
+        finally:
+            time.sleep(random.uniform(0.05, 0.5))
 
 
 def download_image(row, dataset_location, proxy_manager, faker, image_size):
@@ -33,21 +37,26 @@ def download_image(row, dataset_location, proxy_manager, faker, image_size):
     fpath.parent.mkdir(exist_ok=True, parents=True)
     base_url = "https://coverbrowser.com"
     url = f"{base_url}{row['img_url']}"
+    logging.info(url)
     with requests.get(
         url,
         proxies={"http": proxy_manager.get_proxy()},
         headers={"User-Agent": faker.user_agent()},
         stream=True,
+        allow_redirects=True,
+        timeout=(1, 5),
     ) as r:
-        print(r.status_code)
-        image = Image.open(r.raw)
-        image.thumbnail((image_size, image_size))
-        image.save(fpath)
+        if r.status_code == 200:
+            image = Image.open(r.raw)
+            image.thumbnail((image_size, image_size))
+            image.save(fpath)
+        else:
+            logging.info(f"Cannot download {url}. status code: {r.status_code}")
 
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s",
         # datefmt='%m-%d %H:%M',
     )
